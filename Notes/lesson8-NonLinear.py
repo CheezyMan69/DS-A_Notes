644cb88579e98d5ee85e052b98188be8b3c63049
CheezyMan69 <karimaref2005@gmail.com>
2024-06-05 10:57:25 +0200

    Tree Structures added

---
+++ Notes/lesson8-NonLinear.py
@@ -0,0 +1,22 @@
+#Notes -----------------------------------------------------------------------------
+'''
+Non-Linear Data Structures are data structures with multiple branches (like a tree[foreshadowing]),
+they use a hierarchy to sort (on multiple levels[branches]). Uses classes and objects 
+
+- Trees
+    They work on nodes, different nodes on different/multiple levels. some nodes are
+        - Parent nodes 
+            They are the nodes that give access to the next level (to their children)
+        - Child Nodes
+            They are the nodes 
+
+    Each tree consists of a root node and sub trees - children and parents.
+    A node consits of the data inside it and its child nodes.
+
+    Basic operations of Tree Structures:
+        - Create -> creating a tree in the data structure
+        - Insert -> Inserts data into the tree
+        - Search -> Searches the tree to find data
+        - Travesrse -> 
+
+'''
